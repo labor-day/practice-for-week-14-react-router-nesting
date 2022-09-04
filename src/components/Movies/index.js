be7df7ad5d059ba0b6fb9a9dc8a1d94e1374f62e
Scott Shaffer <scottwshaffer1@gmail.com>
2022-09-04 00:36:46 -0600

    practiced nested routers

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,8 +1,28 @@
-function Movies() {
+import { Route, Switch, NavLink } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
-    <div className='comp orange'>
-      <h1>Movies Component</h1>
-    </div>
+    <>
+      <div className="comp orange">
+        <h1>Movies Component</h1>
+        <nav>
+          {movies.map((movie) => {
+            let url = "/movies/".concat(movie.id);
+            return (
+              <NavLink key={movie.id} to={url}>
+                {movie.title}
+              </NavLink>
+            );
+          })}
+        </nav>
+        <Switch>
+          <Route path="/movies/:movieId">
+            <MovieDetails movies={movies} />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
